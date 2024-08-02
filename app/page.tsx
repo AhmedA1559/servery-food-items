e7226f494b4a2abf7f25fb345f08ecc37d5625ee
AhmedA1559 <95593064+AhmedA1559@users.noreply.github.com>
2024-08-01 23:28:22 -0500

    Add link to groupme

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,18 @@ export default async function Home() {
   const data = await fetchMenuData();
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-3xl font-bold mb-8">Rice Servery Viewer</h1>
+      <div className="flex justify-between flex-wrap">
+        <h1 className="text-3xl font-bold mb-8">Rice Servery Viewer</h1>
+        <a
+          href="https://groupme.com/join_group/102053547/xeK5pqzT"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Join GroupMe for automated menu updates
+          </button>
+        </a>
+      </div>
       <Table data={data.weeklyMenu} />
     </div>
   );
