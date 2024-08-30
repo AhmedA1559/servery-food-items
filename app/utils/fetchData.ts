632c87941994fa375cfb5d2ba936bd250a004e09
AhmedA1559 <95593064+AhmedA1559@users.noreply.github.com>
2024-08-30 11:08:02 -0500

    fix user agent

--- app/utils/fetchData.ts
+++ app/utils/fetchData.ts
@@ -107,12 +107,15 @@ const parseWeeklyMenu = async (
   const currentDate = new Date();
 
   const config = {
-    method: 'get',
+    method: "get",
     maxBodyLength: Infinity,
     url: url,
-    headers: { }
+    headers: {
+      "User-Agent":
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.1 Safari/605.1.15",
+    },
   };
-  const response = await axios.request(config)
+  const response = await axios.request(config);
   const html = response.data;
   // get the date of this week's Monday
   const monday = subDays(currentDate, (currentDate.getDay() + 6) % 7);
