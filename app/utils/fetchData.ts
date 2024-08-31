75d380e07f958952b342fb9840fb9fb2197d50d1
AhmedA1559 <95593064+AhmedA1559@users.noreply.github.com>
2024-08-31 11:46:08 -0500

    Change user agent

--- app/utils/fetchData.ts
+++ app/utils/fetchData.ts
@@ -111,6 +111,7 @@ const parseWeeklyMenu = async (
       accept:
         "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
       "accept-language": "en-US,en;q=0.9",
+      "User-Agent": "XY",
     },
     referrerPolicy: "strict-origin-when-cross-origin",
     body: null,
